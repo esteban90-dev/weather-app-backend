da5734375ddb41fb7ab02e285deef33e153d97bb
Stephen Allard <stephencallard@gmail.com>
2022-01-11 20:37:21 -0600

    namespace api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/geocode/', to: 'geocode#show'
-  get '/weather/', to: 'weather#show'
+  namespace :api do 
+    namespace :v1 do 
+      get '/geocode/', to: 'geocode#show'
+      get '/weather/', to: 'weather#show'
+    end
+  end
 end
