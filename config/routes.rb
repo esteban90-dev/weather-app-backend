6c76c498c17e8f27e01c54d2ff2b1c9e85d16299
Stephen Allard <stephencallard@gmail.com>
2022-01-11 20:48:12 -0600

    change namespaced routes to scoped routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do 
-    namespace :v1 do 
+  scope :api do 
+    scope :v1 do 
       get '/geocode/', to: 'geocode#show'
       get '/weather/', to: 'weather#show'
     end
