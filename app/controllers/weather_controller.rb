53e65d62b33df78de5a93a509ed9d2470a39732a
Stephen Allard <stephencallard@gmail.com>
2022-01-13 20:50:29 -0600

    modify weather route to accept a city name instead of a lat/lon

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,6 +1,6 @@
 class WeatherController < ApplicationController
   def show
-    response = RestClient.get 'api.openweathermap.org/data/2.5/weather', {params: {lat: params['lat'], lon: params['lon'], appid: ENV['openweathermap_api_key']}}
+    response = RestClient.get 'api.openweathermap.org/data/2.5/weather', { params: {q: params['q'], appid: ENV['openweathermap_api_key']}}
     render json: response
   end
 end
