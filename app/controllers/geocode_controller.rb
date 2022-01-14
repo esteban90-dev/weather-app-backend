842dabd8c699e755be4ca12ee900d074d365882d
Stephen Allard <stephencallard@gmail.com>
2022-01-13 20:44:24 -0600

    remove geocode route

--- app/controllers/geocode_controller.rb
+++
@@ -1,6 +0,0 @@
-class GeocodeController < ApplicationController
-  def show
-    response = RestClient.get 'http://api.openweathermap.org/geo/1.0/direct', {params: {q: params['q'], limit: params['limit'], appid: ENV['openweathermap_api_key']}}
-    render json: response
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :api do 
     scope :v1 do 
-      get '/geocode/', to: 'geocode#show'
       get '/weather/', to: 'weather#show'
     end
   end
