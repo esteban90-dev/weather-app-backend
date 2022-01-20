bbefa8b3c4169cfd58c6449881268e03f5d5d4a6
Stephen Allard <stephencallard@gmail.com>
2022-01-20 14:42:04 -0600

    add units parameter

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,6 +1,6 @@
 class WeatherController < ApplicationController
   def show
-    response = RestClient.get 'api.openweathermap.org/data/2.5/weather', { params: {q: params['q'], appid: ENV['openweathermap_api_key']}}
+    response = RestClient.get 'api.openweathermap.org/data/2.5/weather', { params: {q: params['q'], units: params['units'], appid: ENV['openweathermap_api_key']}}
     render json: response
   end
 end
